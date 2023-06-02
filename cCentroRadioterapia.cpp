abe69032d5de13848cf3821b48b3f54b6259a468
Lorenzo Mazzante <lorenzomazzante@hotmail.com.ar>
2023-06-02 13:53:42 -0300

    clase centroRadioterapia

---
+++ cCentroRadioterapia.cpp
@@ -0,0 +1,10 @@
+#include "cCentroRadioterapia.h"
+
+cCentroRadioterapia::cCentroRadioterapia(string nombre_centro)
+{
+	this->nombre_centro = nombre_centro;
+}
+
+cCentroRadioterapia::~cCentroRadioterapia()
+{
+}
---
+++ cCentroRadioterapia.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+
+using namespace std;
+
+class cCentroRadioterapia
+{
+public:
+	string nombre_centro;
+
+	cCentroRadioterapia(string nombre_centro);
+	~cCentroRadioterapia();
+
+};
+
