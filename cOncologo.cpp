2780cfd25642558121dccc91f0bb92cd275feff6
alma marquez <almamarquez2003@gmail.com>
2023-06-06 08:48:03 -0300

    idea

--- cOncologo.cpp
+++ cOncologo.cpp
@@ -16,9 +16,13 @@ const string cOncologo::get_DNI()
 
 unsigned int cOncologo::calcular_dosis_sesion(cPaciente p)
 {
-	float dosis = p.get_salud();
+	float dosisnecesaria = p.get_salud();  //IDEA podriamos pasarle tambien la ubicacion del tumor entonces asi sabemoms que radioterapia corresponde
+								 //y asi variamos las dosis entre la cantidad q dice la consigna, yo haria por ejemplo que si la salud esta entre
+								//0 y 0.3 (siendo 0 semi muerto) la dosis va a ser maxima, si esta entre 0.4 y 0.7, la dosis es media y si esta entre
+								//0.8 y 1, la dosis es minima.
 
-	if(dosis)
+
+	//if(dosisnecesaria)
 	
 	return 0;
 }
