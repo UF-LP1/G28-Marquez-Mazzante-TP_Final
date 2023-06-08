c8273b8e4bc770a23de876640a01def6f360892d
Lorenzo Mazzante <lorenzomazzante@hotmail.com.ar>
2023-06-08 16:20:42 -0300

    probando archivos

--- main.cpp
+++ main.cpp
@@ -1,13 +1,47 @@
 #include <iostream>
+#include <vector>
+#include "cCentroRadioterapia.h"
+#include <sstream>
+#include <fstream>
 
 using namespace std;
 
+ifstream leer_archivo(string nombrearchivo, vector<cPaciente*> listaPacientes);
+
 int main()
 {
-    srand(time(NULL));
-    for (int i = 0; i < 10; i++) {
-        int num = 6 + rand() % (8 - 5);
-        cout << num;
-    }
-    return 0;
+	vector<cPaciente*> listaPacientes;
+
 }
+
+
+ifstream leer_archivo(string nombrearchivo, vector<cPaciente*> listaPacientes) {
+	ifstream variablefile;
+	variablefile.open(nombrearchivo, ios::in);
+	if (!(variablefile.is_open()))
+	{
+		cout << "no se pudo abrir el archivo" << endl;
+		exit;
+	}
+
+	string nombreAux;
+	string apellidoAux;
+	string DNIAux;
+	string tel_contactoAux;
+	time_t fecha_nacimientoAux;
+	float saludAux;
+	bool concurrirAux;
+	string coma;
+
+	int i = 0;
+	while (variablefile) {
+		cin >> nombreAux >> coma >> apellidoAux >> coma >> DNIAux >> coma >> tel_contactoAux >> coma
+			>> fecha_nacimientoAux >> coma >> saludAux >> coma >> concurrirAux;
+	}
+
+
+
+
+}
+
+
